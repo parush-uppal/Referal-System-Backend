7cd6075a4fb4e0202ce4b8e9b1c39b756a8a2f79
parush-uppal <parush1008.be21@chitkara.edu.in>
2023-08-05 02:59:43 +0530

    Intial Commit

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = mongoose.Schema({
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    referalLink: {
+      type: String,
+      trim: true,
+      unique: true,
+    },
+    userRefered: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    number: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  });
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    const result = await bcrypt.compare(password, this.password);
+    return result;
+};
+
+module.exports = mongoose.model('User', userSchema)
+
