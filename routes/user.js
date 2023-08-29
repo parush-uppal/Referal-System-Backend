13d2289adaa851bbdcba145c3d685e63d9bf2578
parush-uppal <parush1008.be21@chitkara.edu.in>
2023-08-29 17:02:18 +0530

    Minor Changes

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,12 @@
 const express = require("express");
-const { create, verifyEmail, resendEmailVerificationToken, forgetPassword, sendResetPasswordTokenStatus, resetPassword, signIn, allUser } = require("../controllers/user");
-const { isValidPassResetToken } = require("../middlewares/user");
-const { userValidtor, validate, validatePassword, signInValidator } = require("../middlewares/validator");
+const { create, allUser, singleUser } = require("../controllers/user");
+const { userValidtor, validate} = require("../middlewares/validator");
 
 const router = express.Router();
 
 router.post("/create", userValidtor, validate, create);
-router.post("/sign-in", signInValidator, validate, signIn);
-router.post("/verify-email", verifyEmail);
-router.post("/resend-email-verification-token", resendEmailVerificationToken);
-router.post('/forget-password', forgetPassword)
 router.get("/allUser", allUser); 
-router.post('/verify-pass-reset-token', isValidPassResetToken, sendResetPasswordTokenStatus)
-router.post('/reset-password', validatePassword, validate, isValidPassResetToken, resetPassword)
+router.get("/singleUser", singleUser); 
+
 
 module.exports = router;
